e1fe0cc5b1ad1d17f58bc35be2d18b72f6becccb
Akobir <akobirtojiyev3@gmail.com>
2026-05-30 19:33:57 +0500

    author middleware postController bilan ishlash hullas ko'p ishlar bo'ldi

--- models/post.model.js
+++ models/post.model.js
@@ -2,6 +2,7 @@
 const {Schema, model} = require('mongoose')
 
 const postModel = new Schema({
+    author: {type: Schema.ObjectId, ref: 'User'},//bunda foydalanuvchi ro'yxatdan o'tgan bo'lsa access-token bo'lsa faqat shundan olib author va value sifatida id berish:
     title: {type: String, required: true},
     body: {type: String, required: true},
     picture: {type:String}
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,11 +1,13 @@
 const express = require('express')  
 const postController = require('../controllers/post.controller')
+const authMiddleware = require('../middleware/auth.middleware')
+const authorMiddleware = require('../middleware/author.middleware')
 const router = express.Router()
 
 router.get('/get', postController.getAll)
-router.post('/create', postController.create)
+router.post('/create', authMiddleware, postController.create)
 router.delete('/delete/:id', postController.delete)
-router.put('/put/:id', postController.put)
+router.put('/put/:id', authMiddleware, authorMiddleware, postController.put)
 router.get('/getones/:id', postController.getone)
 
 module.exports = router

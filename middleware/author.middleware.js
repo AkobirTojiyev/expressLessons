69eb9d74f39749b1002356cea8b5043b60de48f8
Akobir <akobirtojiyev3@gmail.com>
2026-06-07 16:26:41 +0500

    add code comm

--- middleware/author.middleware.js
+++ middleware/author.middleware.js
@@ -5,7 +5,7 @@ module.exports = async function (req,res,next) {
     try {
         const post = await postModel.findById(req.params.id)
         const authorId = req.user.id//bu auth.middle ware req ichiga qo'shib bergan user hususiyat bunda foydalanuvchi ro'yxatdan o'tganini tekshiradi va 'req.user'-validatsiyadin o'tkazilgan foydalanuvchini payloadini joylashtiradi.
-        if(post.author.toString() !== authorId){//post author postModelda shu 'author' fieldi bor ObjectId oladi 'req.user'!
+        if(post.author.toString() !== authorId){//post create qilishda qilgan odamni datalarini ushlab qolganmiz shu bilan hozirgi userni access tokenidan yana userini chiqarish
             return next(BaseError.BadRequest('Only author can edit this post'))
         }
         next()
